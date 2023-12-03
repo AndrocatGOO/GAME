43b34acc4a24a76262d934ff200294d1c0823e96
andre <andresfabiangarzonvalencia@gmail.com>
2023-12-03 01:50:48 -0500

    changue pc

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ var config = {
     height: 1000,
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+        autoCenter: Phaser.Scale.CENTER_BOTH,
     },
     physics: {
         default: "arcade",
@@ -26,16 +26,19 @@ var score = 0;
 var scoreText;
 var powerCat = 0;
 var catgirl = null;
+var lastPlatformY = 600;
 
 //Start Game
 var game = new Phaser.Game(config);
 
 //cargar objetos
 function preload() {
-    this.load.image("sky", "assets/sky.png"); //FONDO
+    this.load.image("sky", "assets/Background_2.png");
+    this.load.image("sky2", "assets/sky.png");
     this.load.image("girlPicture", "assets/portrait_female.png"); //girl piture
     this.load.image("catGirlPicture", "assets/portrait only_cat_kigurumi.png"); //cat piture
     this.load.image("ground", "assets/platform.png"); //plataforma
+    this.load.image("backgroundInfo", "assets/backgroundinfo.png"); //plataforma
     this.load.image("star", "assets/star.png"); //estrellas
     this.load.spritesheet("controls", "assets/tilemap_packed.png", {
         //items
@@ -60,30 +63,30 @@ function preload() {
 }
 //crear objetos y funciones del juego
 function create() {
-    //PERSONAJE DESC
-    this.add.image(400, 700, "sky").setTint(0xeeeeff);
-    this.add.image(400, 1100, "sky").setTint(0x000000);
-    girlPicture = this.physics.add.staticGroup();
-    girlPicture.create(100, 700, "girlPicture").setScale(3);
-
-    //cat character
-    catGirlPicture = this.physics.add.staticGroup();
-
     //BACKGROUND
-    this.add.image(400, 300, "sky"); //agregar fondo
+    background = this.add.image(400, 300, "sky");
+    background.setDisplaySize(496 * 4, 272 * 4).setScrollFactor(0);
+    this.tweens.add({
+        targets: background,
+        x: 496,
+        y: 272,
+        duration: 15000,
+        ease: "Linear",
+        yoyo: true,
+        repeat: -1,
+    });
+    background2 = this.add.image(400, 1000, "sky2").setTint(0xdddddd).setDisplaySize(800, 400).setScrollFactor(0);
 
     //PLATAFORMAS
     platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, "ground").setScale(2).refreshBody();
+    platforms.create(0, 568, "ground");
+    platforms.create(800, 568, "ground");
     platforms.create(600, 400, "ground");
     platforms.create(50, 250, "ground");
     platforms.create(750, 220, "ground");
+    platforms.create(400, 50, "ground");
 
-    //PUNTUACION
-    scoreText = this.add.text(200, 616, "Score: 0", {
-        fontSize: "32px",
-        fill: "white",
-    });
+    
 
     //POWER UP GATO
     var catPowerupSprite = this.physics.add.sprite(750, 50, "objects", 0);
@@ -91,7 +94,7 @@ function create() {
     this.physics.add.collider(catPowerupSprite, platforms);
 
     //CONTENEDOR PERSONAJE
-    playerContainer = this.add.container(100, 450);
+    playerContainer = this.add.container(700, 50);
     // Crear a la chica (girl)
     girl = this.physics.add.sprite(0, 0, "girl");
     girl.setSize(24, 20);
@@ -99,6 +102,12 @@ function create() {
     girl.setScale(3);
     this.physics.add.collider(girl, platforms);
     this.physics.add.overlap(girl, catPowerupSprite, collectCat, 0, 1);
+
+    //camera
+    this.cameras.main.setBounds(0, 0, 800, Infinity); 
+    this.cameras.main.startFollow(girl, false, 0.5, 2);
+    this.cameras.main.setZoom(1);
+
     //powerup de gato traje
     catgirl = this.physics.add.sprite(900, 0, "catgirl");
     catgirl.setSize(24, 20);
@@ -155,30 +164,41 @@ function create() {
     //LLAMAR CONTROLES
     cursors = this.input.keyboard.createCursorKeys();
     //controles telefono
-    upButton = this.add.sprite(650, 900, 'controls',757).setScale(8).setInteractive();
+    upButton = this.add
+        .sprite(650, 900, "controls", 757)
+        .setScale(8)
+        .setInteractive()
+        .setScrollFactor(0);
     upButton.setInteractive();
-    upButton.on('pointerdown', function (pointer) {
+    upButton.on("pointerdown", function (pointer) {
         cursors.up.isDown = true;
     });
-    upButton.on('pointerup', function (pointer) {
+    upButton.on("pointerup", function (pointer) {
         cursors.up.isDown = false;
     });
-    leftButton = this.add.sprite(100, 900, 'controls',760).setScale(8).setInteractive();
+    leftButton = this.add
+        .sprite(100, 900, "controls", 760)
+        .setScale(8)
+        .setInteractive()
+        .setScrollFactor(0);
     leftButton.setInteractive();
-    leftButton.on('pointerdown', function (pointer) {
+    leftButton.on("pointerdown", function (pointer) {
         cursors.left.isDown = true;
     });
-    leftButton.on('pointerup', function (pointer) {
+    leftButton.on("pointerup", function (pointer) {
         cursors.left.isDown = false;
     });
-    rightButton = this.add.sprite(300, 900, 'controls',758).setScale(8).setInteractive();
+    rightButton = this.add
+        .sprite(300, 900, "controls", 758)
+        .setScale(8)
+        .setInteractive()
+        .setScrollFactor(0);
     rightButton.setInteractive();
-    leftButton.on('pointerdown', () => {
-    });
-    rightButton.on('pointerdown', function (pointer) {
+    leftButton.on("pointerdown", () => {});
+    rightButton.on("pointerdown", function (pointer) {
         cursors.right.isDown = true;
     });
-    rightButton.on('pointerup', function (pointer) {
+    rightButton.on("pointerup", function (pointer) {
         cursors.right.isDown = false;
     });
     //ESTRELLAS
@@ -198,10 +218,33 @@ function create() {
     bombs = this.physics.add.group();
     this.physics.add.collider(bombs, platforms);
     this.physics.add.collider(bombs, girl, hitBomb, null, this);
+
+    //PERSONAJE DESC
+backgroundInfo = this.add.image(600, 800, "backgroundInfo");
+backgroundInfo.setDisplaySize(170 * 2, 60* 2).setScrollFactor(0).setDepth(1);  // Ajusta la profundidad según sea necesario
+
+girlPicture = this.physics.add.staticGroup();
+girlPicture.create(480, 776, "girlPicture").setScale(1.7).setScrollFactor(0).setDepth(2);  // Ajusta la profundidad según sea necesario
+
+//PUNTUACION
+scoreText = this.add
+    .text(560, 780, "Score: 0", {
+        fontSize: "32px",
+        fill: "black",
+    })
+    .setScrollFactor(0).setDepth(3);  // Ajusta la profundidad según sea necesario
+
+//cat character
+catGirlPicture = this.physics.add.staticGroup();
+
 }
 
 //ACTUALIZAR EVENTOS
 function update() {
+    if (girl.y - lastPlatformY >= 200) {
+        console.log("test1");
+        createRandomPlatform(this, girl.x);
+    }
     //controles para la chica (girl)
     if (cursors.left.isDown) {
         girl.setVelocityX(-160);
@@ -249,9 +292,11 @@ function collectCat(girl, catPowerupSprite) {
     playerContainer.add([catgirl]);
     catgirl.x = girl.x;
     catgirl.y = girl.y - 10;
-
-    
-    catGirlPicture.create(100, 700, "catGirlPicture").setScale(3);
+    catGirlPicture
+        .create(480, 776, "catGirlPicture")
+        .setScale(1.7)
+        .setScrollFactor(0)
+        .setDepth(10);
 }
 //estrellas y puntos
 function collectStar(player, star) {
@@ -282,9 +327,19 @@ function hitBomb(player, bomb) {
     player.anims.play("turn");
     gameOver = 1;
 }
-function evento(){
-    var eventoTeclado = new KeyboardEvent('keydown', {
-        key: 'ArrowRight', 
+function evento() {
+    var eventoTeclado = new KeyboardEvent("keydown", {
+        key: "ArrowRight",
     });
     document.dispatchEvent(eventoTeclado);
+}
+function createPlatform(scene, x, y) {
+    lastPlatformY = y;
+    console.log("test3 " + lastPlatformY);
+    platforms.create(x, y, "ground");
+}
+function createRandomPlatform(scene, playerX) {
+    console.log("test2");
+    var randomX = Phaser.Math.Between(playerX + 700, playerX + 900);
+    createPlatform(scene, randomX, lastPlatformY + 600);
 }
